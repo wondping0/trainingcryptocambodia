f353128967e385da4ff9287e62d06c457cd720fa
wondping0 <faturrohmansugiyarto@gmail.com>
2025-09-14 09:29:34 +0700

    week14

---
+++ week14/CSD/ch2/dist.py
@@ -0,0 +1,22 @@
+from Crypto.Util.number import *
+import random
+
+flag = b'CSDD{REDACTED}'
+
+def genrsa():
+  p = getPrime(512)
+  q = getPrime(512)
+  n = p*q
+  phi = (p-1)*(q-1)
+  e = 65537
+  d = pow(e, -1, phi)
+  return e, d, n
+
+e, d, n = genrsa()
+c1 = pow(bytes_to_long(flag+b'\x44444444444444444'), e, n)
+c2 = pow(bytes_to_long(flag)+random.getrandbits(10), e, n)
+
+print(c1)
+print(c2)
+print(n)
+print(e)
